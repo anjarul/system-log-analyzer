70db4521862c0ac111e511c4df6d2adb94782d3b
anjarul <anjarraj10@gmail.com>
2024-10-28 19:06:52 +0600

    moodified ruote class

--- src/main/scala/com/loganalyzer/routes/ApiRoutes.scala
+++ src/main/scala/com/loganalyzer/routes/ApiRoutes.scala
@@ -2,6 +2,7 @@ package com.loganalyzer.routes
 
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
 import com.loganalyzer.controllers.LogAnalyzerController
 import com.loganalyzer.services.LogAnalyzerService
 import com.loganalyzer.utils.CORSHandler
@@ -12,32 +13,34 @@ class ApiRoutes extends CORSHandler {
   private val service = new LogAnalyzerService()
   private val controller = new LogAnalyzerController(service)
 
-  val route = corsHandler {
-    concat(
-      path("api" / "get_status") {
-        get {
-          complete(StatusCodes.OK -> controller.getStatus)
-        }
-      },
-      path("api" / "get_size") {
-        get {
-          complete(controller.getLogSize)
-        }
-      },
-      path("api" / "data") {
-        post {
-          entity(as[LogRequest]) { request =>
-            complete(controller.analyzeData(request))
+  val route: Route = corsHandler {
+    pathPrefix("api") {
+      concat(
+        path("get_status") {
+          get {
+            complete(StatusCodes.OK -> controller.getStatus)
+          }
+        },
+        path("get_size") {
+          get {
+            complete(controller.getLogSize)
+          }
+        },
+        path("data") {
+          post {
+            entity(as[LogRequest]) { request =>
+              complete(controller.analyzeData(request))
+            }
+          }
+        },
+        path("histogram") {
+          post {
+            entity(as[LogRequest]) { request =>
+              complete(controller.generateHistogram(request))
+            }
           }
         }
-      },
-      path("api" / "histogram") {
-        post {
-          entity(as[LogRequest]) { request =>
-            complete(controller.generateHistogram(request))
-          }
-        }
-      }
-    )
+      )
+    }
   }
 }
