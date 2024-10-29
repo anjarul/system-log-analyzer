ce2f11b7f4a3d4dd1aa317faea310b2c3f20a358
anjarul <anjarraj10@gmail.com>
2024-10-29 15:07:00 +0600

    updated readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # system-log-analyzer
+
+This project is a Scala-based log analysis tool that uses Akka HTTP to expose a REST API for
+processing system logs.
+
+# Requirements
+Make sure you have the following tools installed:
+
+* Java 8 or higher
+* Scala 3.3.4
+* SBT
+* Git
+
+# Setup and Installation
+1. Clone the repository:
+   * git clone git@github.com:anjarul/system-log-analyzer.git
+   * cd systemLogAnalyzer
+
+2. Install dependencies:
+   * sbt update
+
+
+# Configuration
+The application reads configuration values from the application.conf file located in the 
+`src/main/resources/application.conf`
+Make sure to update the **logFilePath** value to point to the correct log file on your system.
--- src/main/scala/com/loganalyzer/MainApp.scala
+++ src/main/scala/com/loganalyzer/MainApp.scala
@@ -1,37 +1,19 @@
 package com.loganalyzer
 
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
 import com.loganalyzer.configurations.AppConfig
 import com.loganalyzer.routes.LogAnalyzerApiRoutes
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.util.{Failure, Success}
-import scala.concurrent.duration._
 
 object MainApp extends App {
-  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "LogAnalyzerSystem")
-  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  implicit val system: ActorSystem = ActorSystem("LogAnalyzerSystem")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val routes = new LogAnalyzerApiRoutes()
-
-  val bindingFuture = Http().newServerAt(AppConfig.host, AppConfig.port)
-    .bind(routes.route)
-
-  bindingFuture.onComplete {
-    case Success(binding) =>
-      val address = binding.localAddress
-      println(s"Server is running at http://${address.getHostString}:${address.getPort}/")
-
-    case Failure(ex) =>
-      println(s"Failed to bind HTTP server: ${ex.getMessage}")
-      system.terminate()
-  }
-
-  sys.addShutdownHook {
-    bindingFuture
-      .flatMap(_.terminate(10.seconds))
-      .onComplete(_ => system.terminate())
-  }
+  Http().newServerAt(AppConfig.host, AppConfig.port).bind(routes.route)
+  println(s"Server is running at http://${AppConfig.host}:${AppConfig.port}/")
 }
