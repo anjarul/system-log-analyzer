f9079f07ae6c90d6ab2daec9963b102fdc722164
anjarul <anjarraj10@gmail.com>
2024-10-29 15:38:14 +0600

    updated mainapp

--- src/main/scala/com/loganalyzer/MainApp.scala
+++ src/main/scala/com/loganalyzer/MainApp.scala
@@ -1,19 +1,40 @@
 package com.loganalyzer
 
-import akka.actor.ActorSystem
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
 import com.loganalyzer.configurations.AppConfig
 import com.loganalyzer.routes.LogAnalyzerApiRoutes
 
 import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
+import scala.concurrent.duration.*
+import scala.io.StdIn
 
 object MainApp extends App {
-  implicit val system: ActorSystem = ActorSystem("LogAnalyzerSystem")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "LogAnalyzerSystem")
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
   val routes = new LogAnalyzerApiRoutes()
-  Http().newServerAt(AppConfig.host, AppConfig.port).bind(routes.route)
-  println(s"Server is running at http://${AppConfig.host}:${AppConfig.port}/")
+
+  private val bindingFuture = Http().newServerAt(AppConfig.host, AppConfig.port)
+    .bind(routes.route)
+
+  bindingFuture.onComplete {
+    case Success(binding) =>
+      val address = binding.localAddress
+      println(s"Server is running at http://${address.getHostString}:${address.getPort}/")
+      println("Press RETURN to stop...")
+
+    case Failure(ex) =>
+      println(s"Failed to bind HTTP server: ${ex.getMessage}")
+      system.terminate()
+  }
+  StdIn.readLine()
+
+  sys.addShutdownHook {
+    bindingFuture
+      .flatMap(_.terminate(10.seconds))
+      .onComplete(_ => system.terminate())
+  }
 }
